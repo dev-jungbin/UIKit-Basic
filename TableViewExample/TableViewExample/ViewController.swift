c0a77c30f5c6fcd67305b4a2db741dd7d06bc383
madilyn <lollin0@naver.com>
2021-01-08 16:22:39 +0900

    day3

---
+++ TableViewExample/TableViewExample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by 1v1 on 2021/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
