90115896425ed24a559e91ab3adbb1885082cae6
madilyn <lollin0@naver.com>
2021-01-17 00:17:58 +0900

    ADD day6 - CollectionViewExample

---
+++ CollectionViewExample/CollectionViewExample/Cell.swift
@@ -0,0 +1,13 @@
+//
+//  cell.swift
+//  CollectionViewExample
+//
+//  Created by 1v1 on 2021/01/16.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+    @IBOutlet weak var btn: UIButton!
+}
